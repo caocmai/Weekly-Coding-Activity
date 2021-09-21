c5ef83086a56c518d6ac4a164be4d554c4fbbdd7
caocmai <caomai@rocketmail.com>
2021-09-21 07:15:56 -0700

    Add method to determine actual memory size

--- src/dev/cao/app/App.java
+++ src/dev/cao/app/App.java
@@ -1,10 +1,20 @@
 package dev.cao.app;
 
+import dev.cao.models.MemorySize;
+
 public class App {
 	
 	
-	
 	public static void main(String[] args) {
+		MemorySize ms = new MemorySize();
+		System.out.println(ms.actualMemorySize("32GB"));
+		System.out.println(ms.actualMemorySize("2GB"));
+		System.out.println(ms.actualMemorySize("512GB"));
+		/* Console Output
+		 * 29.76GB
+		 * 1.86GB
+		 * 476.16GB
+		 */
 		
 	}
 
--- src/dev/cao/models/MemorySize.java
+++ src/dev/cao/models/MemorySize.java
@@ -2,6 +2,17 @@ package dev.cao.models;
 
 public class MemorySize {
 	
-	
+	public String actualMemorySize(String size) {
+	    String type = size.substring(size.length()-2);
+	    String amount = size.substring(0, size.length()-2);
+	    int intAmount = Integer.parseInt(amount);
+
+	    if (type.equals("MB")) {
+	      return String.valueOf(Math.round(intAmount * .93)) + type;
+	    } 
+
+	    double actualSize = intAmount * .93;
+	    return String.format("%.2f", actualSize) + type;
+	  }
 
 }
