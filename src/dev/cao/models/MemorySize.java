f17dea82c9d9314c0468de87cb00987fc2b10bcd
caocmai <caomai@rocketmail.com>
2021-09-21 07:26:27 -0700

    Refactor code

--- src/dev/cao/models/MemorySize.java
+++ src/dev/cao/models/MemorySize.java
@@ -6,12 +6,13 @@ public class MemorySize {
 	    String type = size.substring(size.length()-2);
 	    String amount = size.substring(0, size.length()-2);
 	    int intAmount = Integer.parseInt(amount);
+	    
+	    double actualSize = intAmount * .93;
 
 	    if (type.equals("MB")) {
-	      return String.valueOf(Math.round(intAmount * .93)) + type;
+	      return String.valueOf(Math.round(actualSize)) + type;
 	    } 
-
-	    double actualSize = intAmount * .93;
+	  
 	    return String.format("%.2f", actualSize) + type;
 	  }
 
