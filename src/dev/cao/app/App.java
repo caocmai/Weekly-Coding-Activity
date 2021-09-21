39719f2c20b16222bad9ffe437db2a4ac3a7b833
caocmai <caomai@rocketmail.com>
2021-09-21 09:43:02 -0700

    Refactor to using switch statement

--- src/dev/cao/app/App.java
+++ src/dev/cao/app/App.java
@@ -17,7 +17,7 @@ public class App {
 		 * 476MB
 		 * 930MB
 		 */
-		
 	}
+	
 
 }
--- src/dev/cao/models/MemorySize.java
+++ src/dev/cao/models/MemorySize.java
@@ -5,20 +5,27 @@ public class MemorySize {
 	public String actualMemorySize(String size) {
 	    String type = size.substring(size.length()-2);
 	    String amount = size.substring(0, size.length()-2);
-	    double intAmount = Double.parseDouble(amount);
 	    
+	    double intAmount = Double.parseDouble(amount);
 	    double actualSize = intAmount * .93;
 
 	    if (type.equals("MB")) {
 	      return String.valueOf(Math.round(actualSize)) + type;
 	    }
 	    
-	    if (actualSize <= 1 && type.equals("TB")) {
-	    	return String.valueOf(Math.round(actualSize * 1000)) + "GB";
-	    }
-	    
-	    if (actualSize <= 1 && type.equals("GB")) {
-	    	return String.valueOf(Math.round(actualSize * 1000)) + "MB";
+	    if (actualSize <= 1) {
+	    	String returnType = "";
+	    	switch (type) {
+				case "TB":
+					returnType = "GB";
+					break;
+				case "GB":
+					returnType = "MB";
+					break;
+				default:
+					break;
+			}
+	    	return String.valueOf(Math.round(actualSize * 1000)) + returnType;
 	    }
 
 	    return String.format("%.2f", actualSize) + type;
